a887f2b4550cc7078bcc14cda469a6f8f4ab0481
enzo183 <47628558+enzo183@users.noreply.github.com>
2026-06-06 17:55:21 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {images: {
     unoptimized: true, // <-- C'est ici que tu désactives l'optimisation
-  },};
+  },
+};
 
 export default nextConfig;
